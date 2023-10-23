c4e8f6fb2a051f2e703d9e1170852d187c1fd5e9
Andrii Yasnohorodskyi <yasnogorodskiy@gmail.com>
2023-10-23 21:08:23 +0100

    correct process

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { json } = require('micro');
-const { send } = require('micro');
-const { URL } = require('url');
-
-const postsHandler = require('./src/routes/posts');
-const pagesHandler = require('./src/routes/pages');
-
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3002;
 
-app.use(json());
+app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173' }));
 
-app.get('/api/posts', async (req, res) => {
-  try {
-    const { pathname } = new URL(req.url, 'https://mini-blog-backend.vercel.app');
+app.use('/api/posts', require('./routes/posts'));
+app.use('/api/pages', require('./routes/pages'));
 
-    if (pathname === '/api/posts') {
-      await postsHandler(req, res);
-    } else {
-      send(res, 404, 'Not Found');
-    }
-  } catch (error) {
-    console.error(error);
-    send(res, 500, 'Failed to fetch posts');
-  }
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-app.get('/api/pages', async (req, res) => {
-  try {
-    const { pathname } = new URL(req.url, 'https://mini-blog-backend.vercel.app');
-
-    if (pathname === '/api/pages') {
-      await pagesHandler(req, res);
-    } else {
-      send(res, 404, 'Not Found');
-    }
-  } catch (error) {
-    console.error(error);
-    send(res, 500, 'Failed to fetch pages');
-  }
-});
-
-module.exports = app;
